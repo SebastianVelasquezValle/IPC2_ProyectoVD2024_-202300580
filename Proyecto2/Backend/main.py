efb92063345ecfd1197257cd4e3545a69df8f8c1
Sebastian <sebastianl.velasquezv@gmail.com>
2024-12-22 23:04:59 -0600

    feat: agregando los módulos controladores (admin y usuario), modals y el main

---
+++ Proyecto2/Backend/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+from controllers.usuarioController import BlueprintUsuario
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.register_blueprint(BlueprintUsuario)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=4000, debug=True)
